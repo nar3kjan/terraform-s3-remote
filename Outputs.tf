36061cf4aae5de363ab057c01bc0af5dddb418ec
Narek <zzz@zzz.zzz>
2022-11-12 01:40:24 +0400

    packer-html

--- Outputs.tf
+++ Outputs.tf
@@ -29,7 +29,7 @@ output "aws_vpc_cidrs" {
 output "certificate_id" {
   value = aws_acm_certificate.cert.id
 }
-
+/*
 output "elb-id" {
   value = aws_lb.web.id
 }
@@ -41,3 +41,4 @@ output "elb_zone_id" {
 output "elb_dns_name" {
   value = aws_lb.web.dns_name
 }
+*/
